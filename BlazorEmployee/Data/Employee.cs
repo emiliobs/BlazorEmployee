4debff36cd17f532b8cb7f82a9411043343a8740
Emilio <barrera_emilio@yahoo.es>
2020-02-01 15:48:07 +0000

    Add project files.

---
+++ BlazorEmployee/Data/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEmployee.Data
+{
+    public class Employee
+    {
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+    }
+}
---
+++ BlazorEmployee/Services/EmployeeService.cs
@@ -0,0 +1,53 @@
+﻿using BlazorEmployee.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEmployee.Services
+{
+    public class EmployeeService
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //for CRUD operation
+
+        //GEt all Employee
+        public List<Employee> GetAllEmployee() => _context.Employees.ToList();
+
+        //Insert Employee
+        public string Create(Employee employee)
+        {
+            _context.Add(employee);
+            _context.SaveChanges();
+            return "Save Successfully";
+        }
+
+        //Get Employye by ID:
+        public Employee GetEmployeeById(int id) => _context.Employees.FirstOrDefault(e => e.Id == id);
+
+        //Update Employee
+        public string UpdateEmpoyee(Employee employee)
+        {
+            _context.Update(employee);
+            _context.SaveChanges();
+            return "Update Successfully";
+        }
+
+        //Delete Employee:
+        public string deleteEmployee(Employee employee)
+        {
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return "Delete Successfully";
+        }
+       
+
+    }
+}
